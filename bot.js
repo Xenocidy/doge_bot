51e1306b5f2a4d42772425ad0a970a574c9fefae
Jacky Wong <jacky@Jackys-MacBook-Air.local>
2021-12-25 15:43:36 -0500

    Visual update

--- bot.js
+++ bot.js
@@ -12,13 +12,13 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MES
 // on startup
 bot.on('ready', async () => {
     console.log(`${bot.user.username} is online`);
-    
+
     // Get crypto price from coingecko API
     const { data } = await axios.get(
         `https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd`
     );
 
-    bot.user.setActivity(`DOGE @ $${data.dogecoin.usd}`, { type: "WATCHING" });
+    bot.user.setActivity(`STONKS`, { type: "PLAYING" });
 });
 
 bot.on('messageCreate', async (message) => {
